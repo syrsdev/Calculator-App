82f4dfc75132fe4e3a262ad8d9a335ef6c849c7e
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-01-26 20:07:24 +0700

    change bg splash

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -14,7 +14,8 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => HomeScreen()));
+          // ignore: use_build_context_synchronously
+          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
     });
   }
 
